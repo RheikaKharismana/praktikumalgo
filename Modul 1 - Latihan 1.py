e4fdede466c47210152519e1d6958840b59f36ac
RheikaKharismana <48577266+RheikaKharismana@users.noreply.github.com>
2022-10-19 13:00:48 +0700

    Add files via upload

---
+++ Modul 1 - Latihan 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@Materi: Variabel dan Pemrograman Dasar
+@NIM: 064001600006
+@author: Rheika Kharismana
+"""
+
+nama = input("nama kamu siapa? ")
+asal = input("darimana asal kamu? " + nama + "? ")
+sekolah = input("sebelumnya bersekolah dimana? ")
+
+print("hallo selamat siang " + nama)
+print("Wah dari " + asal)
+print("Pasti menyenangkan bersekolah di " + sekolah)
---
+++ Modul 1 - Latihan 2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@Materi: Variabel dan Pemrograman Dasar
+@NIM: 064001600006
+@author: Rheika Kharismana
+"""
+
+print("Hitung Luas Ruangan")
+
+panjang = float(input("Masukkan Panjang Ruangan: "))
+lebar = float(input("Masukkan Lebar Ruangan: "))
+satuan = input("Masukkan Satuan (Meter/Inci): ")
+
+hasil = panjang * lebar
+
+print("Luas ruangan dengan panjang ", panjang, " dan lebar ", lebar, " adalah ", hasil, " ", satuan)
